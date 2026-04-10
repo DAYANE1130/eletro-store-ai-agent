807959e65df5fbca73af46c11981a0e8f188ccd5
Dayane <dayanebmartins@gmail.com>
2026-04-10 18:53:02 -0300

    refactor:

--- app.py
+++ app.py
@@ -7,34 +7,28 @@ import gradio as gr
 
 app = Flask(__name__)
 
-# criando função de bate papo para  enviar ao Gradio
-
-
 def get_response(message, _history):
-    time.sleep(5)  # Espera 5 segundos antes de CADA chamada
+    #time.sleep(5)  # Controle de cota chamadas à Api
     try:
         text = message['text']
         prompt = [text]
-        file = upload_file(message)  # file é uma lista
+        file = upload_file(message)
         prompt.extend(file)
         response = chat.send_message(prompt)
-        print("SOU A RESPOSTA da função de bate papo", response.text)
     except InvalidArgument as e:
         print(f"Erro: {e}")
         response = chat.send_message(
             f"O usuário te enviou um arquivo para você ler e obteve o erro: {e}.Pode explicar o que houve e dizer quais tipos de arquivos você dá suporte? Assuma que a pessoa não sabe programação e não quer ver o erro original. Explique de forma simples e concisa."
         )
     return response.text
+    
 
-
-# INTEGRANDO COM O GRADIO -testando primeiro contato
 gr.ChatInterface(
-    fn=get_response,  
+    fn=get_response,
     title="Assistente Virtual Eletro Store",
+    show_progress=False,
     multimodal=True
-    ).launch()
-
-# Rotas básicas
+).launch()
 
 
 @app.route('/')
